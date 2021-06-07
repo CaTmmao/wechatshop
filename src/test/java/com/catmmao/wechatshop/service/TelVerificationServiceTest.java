946caac3f8ed2196989a9f84f7853ea1101e2631
catmmao <catmmao@gmail.com>
2021-06-07 22:18:17 +0800

    test(telVerificationService): 添加手机验证单元测试

---
+++ src/test/java/com/catmmao/wechatshop/service/TelVerificationServiceTest.java
@@ -0,0 +1,26 @@
+package com.catmmao.wechatshop.service;
+
+import com.catmmao.wechatshop.model.TelAndCode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TelVerificationServiceTest {
+    private static TelAndCode correctTelFormat = new TelAndCode("13544444444", null);
+    private static TelAndCode noTel = new TelAndCode(null, null);
+
+    @Test
+    public void returnIfValid() {
+        // TelVerificationService 没有任何依赖，因此直接实例化就可以了
+        Assertions.assertTrue(new TelVerificationService().verifyTel(correctTelFormat));
+    }
+
+    @Test
+    public void noTelField() {
+        Assertions.assertFalse(new TelVerificationService().verifyTel(noTel));
+    }
+
+    @Test
+    public void noParam() {
+        Assertions.assertFalse(new TelVerificationService().verifyTel(null));
+    }
+}
