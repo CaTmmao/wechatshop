affa08cf7c9a70203f7cea988eb88124270f8ece
catmmao <catmmao@gmail.com>
2021-06-10 22:29:07 +0800

    feat(model/response): 用户登录相关接口响应体的模型

---
+++ src/main/java/com/catmmao/wechatshop/model/response/UserLoginResponseModel.java
@@ -0,0 +1,37 @@
+package com.catmmao.wechatshop.model.response;
+
+import com.catmmao.wechatshop.model.generated.User;
+
+public class UserLoginResponseModel {
+    private Boolean isLogin;
+    private User user;
+
+    private UserLoginResponseModel(boolean isLogin, User user) {
+        this.isLogin = isLogin;
+        this.user = user;
+    }
+
+    public static UserLoginResponseModel notLogin() {
+        return new UserLoginResponseModel(false, null);
+    }
+
+    public static UserLoginResponseModel loggedIn(User user) {
+        return new UserLoginResponseModel(true, user);
+    }
+
+    public boolean isLogin() {
+        return isLogin;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setLogin(Boolean login) {
+        isLogin = login;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
