22cbe8a310af4ca3718503043f29baaada8d353e
catmmao <catmmao@gmail.com>
2021-06-13 13:07:52 +0800

    style(checkstyle): 格式调整

--- src/main/java/com/catmmao/wechatshop/WechatshopApplication.java
+++ src/main/java/com/catmmao/wechatshop/WechatshopApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WechatshopApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WechatshopApplication.class, args);
-		System.out.println("ok");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WechatshopApplication.class, args);
+        System.out.println("ok");
+    }
 }
