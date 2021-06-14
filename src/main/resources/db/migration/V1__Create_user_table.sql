b2510b8c5782fcf9569f2ec65234ca51228af0f1
catmmao <catmmao@gmail.com>
2021-06-14 14:55:34 +0800

    chore: 数据库表中的 update_date 字段调整为非必填

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE `user` (
-    id bigint primary key auto_increment,
-    name varchar(100),
-    tel varchar(100) unique,
+CREATE TABLE `user`
+(
+    id         bigint primary key auto_increment,
+    name       varchar(100),
+    tel        varchar(100) unique,
     avatar_url varchar(1024),
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    updated_at timestamp ON UPDATE NOW()
 )
