cfe0931ba20ab4e93b9e1dc96db30ff5481f26c9
catmmao <catmmao@gmail.com>
2021-06-18 17:13:20 +0800

    refactor(集成测试): 封装重复部分

---
+++ src/test/java/com/catmmao/wechatshop/integration/AbstractIntegrationTest.java
@@ -0,0 +1,90 @@
+package com.catmmao.wechatshop.integration;
+
+import java.util.List;
+
+import com.catmmao.wechatshop.model.TelAndCode;
+import com.catmmao.wechatshop.model.response.UserLoginResponseModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(properties = {"spring.config.location=classpath:test-application.yml"})
+public class AbstractIntegrationTest {
+    protected static final TelAndCode CORRECT_TEL = new TelAndCode("13544444444", null);
+    protected static final TelAndCode NULL_PARAM = new TelAndCode(null, null);
+    protected static final TelAndCode CORRECT_ALL_PARAM = new TelAndCode("13544444444", "000000");
+
+    @LocalServerPort
+    protected int port;
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    protected String getUrl(String api) {
+        return "http://localhost:" + port + "/api/v1" + api;
+    }
+
+    /**
+     * 获取 session id
+     *
+     * @param cookie 格式 -> "JSESSIONID=0c3a114e-3a59-4631-8ae0-2051ba3d375f; Path=/; HttpOnly; SameSite=lax"
+     * @return sessionId 格式 -> "JSESSIONID=0c3a114e-3a59-4631-8ae0-2051ba3d375f"
+     */
+    protected String getSessionId(String cookie) {
+        int endIndex = cookie.indexOf(";");
+        return cookie.substring(0, endIndex);
+    }
+
+    /**
+     * 查看登录状态
+     *
+     * @param entity  请求内容,包括 header 和 body
+     * @param ifLogin 断言状态是否登录
+     */
+    protected void checkLoginStatus(HttpEntity<?> entity, boolean ifLogin) {
+        ResponseEntity<UserLoginResponseModel> response =
+            restTemplate.exchange(getUrl("/session"), HttpMethod.GET, entity, UserLoginResponseModel.class);
+
+        // 已登录
+        if (ifLogin) {
+            Assertions.assertTrue(response.getBody().isLogin());
+        } else {
+            Assertions.assertFalse(response.getBody().isLogin());
+        }
+    }
+
+    // 登录 && 返回登录后获取的 sessionId
+    protected String loginAndGetSessionId() {
+        //1.查看登录状态: 发送 "/api/session" get 请求,返回未登录状态
+        checkLoginStatus(null, false);
+
+        //2.获取手机号验证码: 发送 "/api/code" post 请求,参数是手机号
+        ResponseEntity<String> response2 = restTemplate.postForEntity(getUrl("/code"), CORRECT_TEL, String.class);
+        Assertions.assertEquals(HttpStatus.OK, response2.getStatusCode());
+
+
+        //3.登录: 发送 "/api/session" post 请求,参数是手机号和手机验证码,登录后获取 cookie
+        ResponseEntity<String> response3 =
+            restTemplate.postForEntity(getUrl("/session"), CORRECT_ALL_PARAM, String.class);
+
+        List<String> setCookieHeaderValues = response3.getHeaders().get(HttpHeaders.SET_COOKIE);
+        Assertions.assertNotNull(setCookieHeaderValues);
+        String cookie = setCookieHeaderValues
+            .stream()
+            .filter(i -> i.contains("JSESSIONID"))
+            .findFirst()
+            .get();
+
+        return getSessionId(cookie);
+    }
+}
--- src/test/java/com/catmmao/wechatshop/integration/AuthIntegrationTest.java
+++ src/test/java/com/catmmao/wechatshop/integration/AuthIntegrationTest.java
@@ -1,18 +1,12 @@
 package com.catmmao.wechatshop.integration;
 
-import java.util.List;
-
 import com.catmmao.wechatshop.WechatshopApplication;
-import com.catmmao.wechatshop.model.TelAndCode;
-import com.catmmao.wechatshop.model.response.UserLoginResponseModel;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -21,46 +15,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 // AuthController 相关的集成测试
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = WechatshopApplication.class, // 启动应用的类
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // 指定应用端口
-//@TestPropertySource(locations = "classpath:application.yml")
-public class AuthIntegrationTest {
-    private static final TelAndCode CORRECT_ALL_PARAM = new TelAndCode("13544444444", "000000");
-    private static final TelAndCode CORRECT_TEL = new TelAndCode("13544444444", null);
-    private static final TelAndCode NULL_PARAM = new TelAndCode(null, null);
-
-    @LocalServerPort
-    private int port;
-
+public class AuthIntegrationTest extends AbstractIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    private String sendCodeApiUrl;
-
-    private String sessionApiUrl;
-
-    @BeforeEach
-    void setup() {
-        sendCodeApiUrl = getUrl("/code");
-        sessionApiUrl = getUrl("/session");
-    }
-
-    private String getUrl(String api) {
-        return "http://localhost:" + port + "/api/v1" + api;
-    }
-
     @Test
     public void testSendCodeWithValidParamReturnOk() {
-        ResponseEntity<String> response = restTemplate.postForEntity(sendCodeApiUrl, CORRECT_ALL_PARAM, String.class);
-
+        ResponseEntity<String> response = restTemplate.postForEntity(getUrl("/code"), CORRECT_ALL_PARAM, String.class);
         Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
     public void testSendCodeWithNoTelParamReturnBadRequest() {
-        ResponseEntity<String> response = restTemplate.postForEntity(sendCodeApiUrl, NULL_PARAM, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(getUrl("/code"), NULL_PARAM, String.class);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
@@ -85,57 +55,21 @@ public class AuthIntegrationTest {
             6.查看登录状态: 发送 "/api/session" get 请求,在 header 添加 cookie,返回未登录状态
         */
 
-
-        //1.查看登录状态: 发送 "/api/session" get 请求,返回未登录状态
-        ResponseEntity<UserLoginResponseModel> response1 =
-            restTemplate.getForEntity(sessionApiUrl, UserLoginResponseModel.class);
-        Assertions.assertFalse(response1.getBody().isLogin());
-
-        //2.获取手机号验证码: 发送 "/api/code" post 请求,参数是手机号
-        ResponseEntity<String> response2 = restTemplate.postForEntity(sendCodeApiUrl, CORRECT_TEL, String.class);
-        Assertions.assertEquals(HttpStatus.OK, response2.getStatusCode());
-
-
-        //3.登录: 发送 "/api/session" post 请求,参数是手机号和手机验证码,登录后获取 cookie
-        ResponseEntity<String> response3 = restTemplate.postForEntity(sessionApiUrl, CORRECT_ALL_PARAM, String.class);
-
-        List<String> setCookieHeaderValues = response3.getHeaders().get(HttpHeaders.SET_COOKIE);
-        Assertions.assertNotNull(setCookieHeaderValues);
-        String cookie = setCookieHeaderValues
-            .stream()
-            .filter(i -> i.contains("JSESSIONID"))
-            .findFirst()
-            .get();
-
-        String sessionId = getSessionId(cookie);
+        //1&2&3
+        String sessionId = loginAndGetSessionId();
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cookie", sessionId);
+        HttpEntity<?> requestEntity = new HttpEntity<>(null, headers);
 
         //4.查看登录状态: 发送 "/api/session" get 请求,在 header 添加 cookie,返回已登录状态
-        HttpEntity<?> requestEntity = new HttpEntity<>(null, headers);
-        ResponseEntity<UserLoginResponseModel> response4 =
-            restTemplate.exchange(sessionApiUrl, HttpMethod.GET, requestEntity, UserLoginResponseModel.class);
-        Assertions.assertTrue(response4.getBody().isLogin());
+        checkLoginStatus(requestEntity, true);
 
         //5.登出: 发送 "/api/session" delete 请求,在 header 添加 cookie
         ResponseEntity<String> response5 =
-            restTemplate.exchange(sessionApiUrl, HttpMethod.DELETE, requestEntity, String.class);
+            restTemplate.exchange(getUrl("/session"), HttpMethod.DELETE, requestEntity, String.class);
         Assertions.assertEquals(HttpStatus.OK, response5.getStatusCode());
 
         //6.查看登录状态: 发送 "/api/session" get 请求,在 header 添加 cookie,返回未登录状态
-        ResponseEntity<UserLoginResponseModel> response6 =
-            restTemplate.exchange(sessionApiUrl, HttpMethod.GET, requestEntity, UserLoginResponseModel.class);
-        Assertions.assertFalse(response6.getBody().isLogin());
-    }
-
-    /**
-     * 获取 session id
-     *
-     * @param cookie 格式 -> "JSESSIONID=0c3a114e-3a59-4631-8ae0-2051ba3d375f; Path=/; HttpOnly; SameSite=lax"
-     * @return sessionId 格式 -> "JSESSIONID=0c3a114e-3a59-4631-8ae0-2051ba3d375f"
-     */
-    private String getSessionId(String cookie) {
-        int endIndex = cookie.indexOf(";");
-        return cookie.substring(0, endIndex);
+        checkLoginStatus(requestEntity, false);
     }
 }
