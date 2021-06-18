5da72541b3f7fb10626dea94cf952aa19ea45c34
catmmao <catmmao@gmail.com>
2021-06-18 16:56:38 +0800

    chore: goods 接口返回的响应体的模型

--- src/main/java/com/catmmao/wechatshop/model/response/GoodsResponseModel.java
+++ src/main/java/com/catmmao/wechatshop/model/response/GoodsResponseModel.java
@@ -3,10 +3,36 @@ package com.catmmao.wechatshop.model.response;
 import com.catmmao.wechatshop.model.generated.Goods;
 
 public class GoodsResponseModel {
+    private String message;
     private Goods data;
 
-    public GoodsResponseModel(Goods data) {
+    public GoodsResponseModel(String message, Goods data) {
+        this.message = message;
         this.data = data;
+    }
+
+    public static GoodsResponseModel createdSuccess(Goods goods) {
+        return new GoodsResponseModel(null, goods);
+    }
+
+    public static GoodsResponseModel deleteSuccess(Goods goods) {
+        return new GoodsResponseModel(null, goods);
+    }
+
+    public static GoodsResponseModel forbiddenForShop(String message) {
+        return new GoodsResponseModel(message, null);
+    }
+
+    public static GoodsResponseModel notFound(String message) {
+        return new GoodsResponseModel(message, null);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     public Goods getData() {
