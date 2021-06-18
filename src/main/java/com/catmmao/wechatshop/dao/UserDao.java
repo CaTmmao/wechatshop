7f5bd10560fb708ac23dff56ebb214b354a7d0ec
catmmao <catmmao@gmail.com>
2021-06-18 17:09:06 +0800

    refactor(dao/user): 直接使用 mapper
    需要额外引用 mybatis-generator 相关插件,已引入完毕,插件叫 org.mybatis.generator.plugins.MapperAnnotationPlugin

--- src/main/java/com/catmmao/wechatshop/dao/UserDao.java
+++ src/main/java/com/catmmao/wechatshop/dao/UserDao.java
@@ -5,33 +5,24 @@ import java.util.List;
 import com.catmmao.wechatshop.dao.mapper.UserMapper;
 import com.catmmao.wechatshop.model.generated.User;
 import com.catmmao.wechatshop.model.generated.UserExample;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao {
-    private final SqlSessionFactory sqlSessionFactory;
+    private final UserMapper userMapper;
 
-    public UserDao(SqlSessionFactory sqlSessionFactory) {
-        this.sqlSessionFactory = sqlSessionFactory;
-    }
-
-    public void insertUser(User user) {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
-            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-            userMapper.insert(user);
-        }
+    public UserDao(UserMapper userMapper) {
+        this.userMapper = userMapper;
     }
 
     public User getUserByTel(String tel) {
-        try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
-            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andTelEqualTo(tel);
+        List<User> list = userMapper.selectByExample(userExample);
 
-            UserExample userExample = new UserExample();
-            userExample.createCriteria().andTelEqualTo(tel);
-            List<User> list = userMapper.selectByExample(userExample);
-            return list.get(0);
+        if (list.isEmpty()) {
+            return null;
         }
+        return list.get(0);
     }
 }
--- src/main/java/com/catmmao/wechatshop/service/UserService.java
+++ src/main/java/com/catmmao/wechatshop/service/UserService.java
@@ -4,16 +4,19 @@ import java.util.Date;
 import java.util.Optional;
 
 import com.catmmao.wechatshop.dao.UserDao;
+import com.catmmao.wechatshop.dao.mapper.UserMapper;
 import com.catmmao.wechatshop.model.generated.User;
-import org.apache.ibatis.exceptions.PersistenceException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserDao userDao;
+    private final UserMapper userMapper;
 
-    public UserService(UserDao userDao) {
+    public UserService(UserDao userDao, UserMapper userMapper) {
         this.userDao = userDao;
+        this.userMapper = userMapper;
     }
 
     public User createUserIfNotExist(String tel) {
@@ -23,7 +26,7 @@ public class UserService {
 
         // 没有判断用户是否存在在数据库的步骤，因为是多线程运行，可能会有问题，所以这里直接将用户数据插入到数据库
         try {
-            userDao.insertUser(user);
+            userMapper.insert(user);
         } catch (DuplicateKeyException e) {
             return userDao.getUserByTel(tel);
         } catch (Exception e) {
