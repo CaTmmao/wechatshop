1f1012b0bb2a7f999927957a7deb62a5d49c1dd8
catmmao <catmmao@gmail.com>
2021-06-18 17:28:22 +0800

    feat(商品相关): 完善商品接口

--- src/main/java/com/catmmao/wechatshop/controller/GoodsController.java
+++ src/main/java/com/catmmao/wechatshop/controller/GoodsController.java
@@ -1,15 +1,23 @@
 package com.catmmao.wechatshop.controller;
 
 import java.util.Date;
+import java.util.List;
 
+import com.catmmao.wechatshop.exception.ForbiddenForShopException;
+import com.catmmao.wechatshop.exception.ResourceNotFoundException;
 import com.catmmao.wechatshop.model.generated.Goods;
 import com.catmmao.wechatshop.model.response.GoodsResponseModel;
+import com.catmmao.wechatshop.model.response.PaginationResponseModel;
 import com.catmmao.wechatshop.service.GoodsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,9 +32,40 @@ public class GoodsController {
     @PostMapping("/goods")
     public ResponseEntity<GoodsResponseModel> createGoods(@RequestBody Goods goods) {
         sanitize(goods);
-        goods = goodsService.createGoods(goods);
-        GoodsResponseModel data = new GoodsResponseModel(goods);
-        return new ResponseEntity<>(data, HttpStatus.CREATED);
+        try {
+            goods = goodsService.createGoods(goods);
+            GoodsResponseModel responseBody = GoodsResponseModel.createdSuccess(goods);
+            return new ResponseEntity<>(responseBody, HttpStatus.CREATED);
+        } catch (ForbiddenForShopException e) {
+            GoodsResponseModel responseBody = GoodsResponseModel.forbiddenForShop(e.getMessage());
+            return new ResponseEntity<>(responseBody, HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @DeleteMapping("/goods/{id}")
+    public ResponseEntity<GoodsResponseModel> deleteGoods(@PathVariable("id") Long goodsId) {
+        GoodsResponseModel responseBody;
+
+        try {
+            Goods goods = goodsService.deleteGoodsByGoodsId(goodsId);
+            responseBody = GoodsResponseModel.deleteSuccess(goods);
+            return new ResponseEntity<>(responseBody, HttpStatus.NO_CONTENT);
+        } catch (ForbiddenForShopException e) {
+            responseBody = GoodsResponseModel.forbiddenForShop(e.getMessage());
+            return new ResponseEntity<>(responseBody, HttpStatus.FORBIDDEN);
+        } catch (ResourceNotFoundException e) {
+            responseBody = GoodsResponseModel.notFound(e.getMessage());
+            return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/goods")
+    public ResponseEntity<PaginationResponseModel<List<Goods>>> getGoodsListByShopId(@RequestParam Integer pageNum,
+                                                                                     @RequestParam Integer pageSize,
+                                                                                     @RequestParam(required = false)
+                                                                                         Integer shopId) {
+        PaginationResponseModel<List<Goods>> responseBody = goodsService.getGoodsByShopId(pageNum, pageSize, shopId);
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 
     /**
--- src/main/java/com/catmmao/wechatshop/service/GoodsService.java
+++ src/main/java/com/catmmao/wechatshop/service/GoodsService.java
@@ -1,18 +1,113 @@
 package com.catmmao.wechatshop.service;
 
-import com.catmmao.wechatshop.dao.GoodsDao;
+import java.util.List;
+
+import com.catmmao.wechatshop.UserContext;
+import com.catmmao.wechatshop.dao.mapper.GoodsMapper;
+import com.catmmao.wechatshop.dao.mapper.ShopMapper;
+import com.catmmao.wechatshop.exception.ForbiddenForShopException;
+import com.catmmao.wechatshop.exception.ResourceNotFoundException;
+import com.catmmao.wechatshop.model.DbDataStatus;
 import com.catmmao.wechatshop.model.generated.Goods;
+import com.catmmao.wechatshop.model.generated.GoodsExample;
+import com.catmmao.wechatshop.model.generated.Shop;
+import com.catmmao.wechatshop.model.response.PaginationResponseModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GoodsService {
-    private final GoodsDao goodsDao;
+    private final GoodsMapper goodsMapper;
+    private final ShopMapper shopMapper;
 
-    public GoodsService(GoodsDao goodsDao) {
-        this.goodsDao = goodsDao;
+
+    public GoodsService(GoodsMapper goodsMapper,
+                        ShopMapper shopMapper) {
+        this.goodsMapper = goodsMapper;
+        this.shopMapper = shopMapper;
     }
 
+    /**
+     * 创建商品
+     * @param goods 商品信息
+     * @return 创建好的商品信息
+     */
     public Goods createGoods(Goods goods) {
-        return goodsDao.insert(goods);
+        Shop shop = shopMapper.selectByPrimaryKey(goods.getShopId());
+
+        Long userId = UserContext.getCurrentUser().getId();
+        if (userId.equals(shop.getOwnerUserId())) {
+            long id = goodsMapper.insert(goods);
+            goods.setId(id);
+            return goods;
+        } else {
+            throw new ForbiddenForShopException("店铺不属于该用户");
+        }
+    }
+
+    /**
+     * 删除商品
+     * @param goodsId 商品 id
+     * @return 已删除的商品信息
+     */
+    public Goods deleteGoodsByGoodsId(Long goodsId) {
+        Goods goods = goodsMapper.selectByPrimaryKey(goodsId);
+
+        if (goods == null) {
+            throw new ResourceNotFoundException("商品未找到！");
+        }
+
+        Shop shop = shopMapper.selectByPrimaryKey(goods.getShopId());
+        Long userId = UserContext.getCurrentUser().getId();
+        if (!userId.equals(shop.getOwnerUserId())) {
+            throw new ForbiddenForShopException("店铺不属于该用户");
+        }
+
+        goods.setStatus(DbDataStatus.DELETE.getName());
+        goodsMapper.updateByPrimaryKey(goods);
+        return goods;
+    }
+
+    /**
+     * 获取所有商品
+     *
+     * @param pageNum  当前页数，从1开始
+     * @param pageSize 每页显示的数量
+     * @param shopId   店铺ID，若传递，则只显示该店铺中的商品
+     * @return 所有商品
+     */
+    public PaginationResponseModel<List<Goods>> getGoodsByShopId(
+        Integer pageNum, Integer pageSize, Integer shopId) {
+        // 商品总数量
+        int totalNumber = countGoods(shopId);
+        // 商品总页数
+        int totalPage = totalNumber % pageSize == 0 ? totalNumber / pageSize : totalNumber / pageSize + 1;
+
+        // 获取所有商品
+        GoodsExample page = new GoodsExample();
+        page.setLimit(pageSize);
+        page.setOffset((pageNum - 1) * pageSize);
+        List<Goods> goodsList = goodsMapper.selectByExample(page);
+
+        return new PaginationResponseModel<>(pageSize, pageNum, totalPage, goodsList);
+    }
+
+    /**
+     * 计算商品总数量
+     * @param shopId 店铺 id
+     * @return 商品总数量
+     */
+    public int countGoods(Integer shopId) {
+        GoodsExample goodsExample = new GoodsExample();
+
+        // 没有商店 id,获取所有商店的商品
+        if (shopId == null) {
+            goodsExample.createCriteria().andStatusEqualTo(DbDataStatus.OK.getName());
+        } else {
+            goodsExample.createCriteria()
+                .andStatusEqualTo(DbDataStatus.OK.getName())
+                .andShopIdEqualTo(shopId.longValue());
+        }
+
+        return (int) goodsMapper.countByExample(goodsExample);
     }
 }
