0f9302da522b6d290371c2d4e8d1175047296126
catmmao <catmmao@gmail.com>
2021-07-11 19:17:32 +0800

    feat(wechatshop-main): user 表中添加 address 字段

--- wechatshop-main/src/main/resources/db/migration/V1__Create_user_table.sql
+++ wechatshop-main/src/main/resources/db/migration/V1__Create_user_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE `user`
     name       varchar(100),
     tel        varchar(100) unique,
     avatar_url varchar(1024),
+    address    varchar(1024),
     created_at timestamp NOT NULL DEFAULT NOW(),
     updated_at timestamp ON UPDATE NOW()
 );
